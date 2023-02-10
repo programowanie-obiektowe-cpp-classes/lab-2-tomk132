9c4e75d6926e803c07a6dc7acbc8bdfe1379709c
kucin <kucin@LAPTOP-2VJU9K9D>
2023-02-10 11:57:18 +0100

    Rozwiązanie lab 2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -5,4 +5,54 @@
 class ResourceManager
 {
     // Twoja implementacja tutaj
+ 
+public: 
+    Resource *obj;
+
+
+ResourceManager() : obj{new Resource} { };
+
+ResourceManager(const ResourceManager& other)
+    : obj{new Resource{*other.obj}} {       
+};
+  
+ResourceManager& operator=(const ResourceManager& other)
+{
+    *obj = *other.obj; 
+    return *this;
+}
+
+
+ResourceManager(ResourceManager&& other) noexcept : obj{new Resource{std::move(*other.obj)}}
+{
+    //obj = new Resource;
+    //*obj = std::move(*other.obj);
+    delete other.obj;
+    other.obj = nullptr; 
+    
+}
+
+
+ResourceManager& operator=(ResourceManager&& other) noexcept
+{
+    *obj = std::move(*other.obj); 
+    delete other.obj; 
+    other.obj = nullptr; 
+
+    return *this;
+}
+
+~ResourceManager() 
+{ 
+    delete obj;
+    printf("ResourceManager Destructor \n");
+}; 
+
+double get() 
+{
+    return obj->get(); 
+}; 
+
+
+
 };
